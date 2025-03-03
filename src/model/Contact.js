a93d6c086c55bc553490473b9b135589c2c00bfe
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-03-03 23:50:45 +0530

    [Aakrati] add. UC2 done

--- src/model/Contact.js
+++ src/model/Contact.js
@@ -1,13 +1,41 @@
 class Contact {
     constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.firstName = this.validateName(firstName);
+        this.lastName = this.validateName(lastName);
         this.address = address;
         this.city = city;
         this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
+        this.zip = this.validateZip(zip);
+        this.phoneNumber = this.validatePhoneNumber(phoneNumber);
+        this.email = this.validateEmail(email);
+    }
+
+    validateName(name) {
+        if (!/^[A-Z][a-zA-Z]{2,}$/.test(name)) {
+            throw new Error("Invalid Name! Name should start with a capital letter and have at least 3 characters.");
+        }
+        return name;
+    }
+
+    validateZip(zip) {
+        if (!/^\d{5}(-\d{4})?$/.test(zip)) {
+            throw new Error("Invalid Zip Code! Must be in 5-digit or 9-digit (ZIP+4) format.");
+        }
+        return zip;
+    }
+
+    validatePhoneNumber(phoneNumber) {
+        if (!/^\d{10}$/.test(phoneNumber)) {
+            throw new Error("Invalid Phone Number! Must be exactly 10 digits.");
+        }
+        return phoneNumber;
+    }
+
+    validateEmail(email) {
+        if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+            throw new Error("Invalid Email Address!");
+        }
+        return email;
     }
 
     display() {
@@ -17,4 +45,5 @@ class Contact {
         console.log(`Email: ${this.email}`);
     }
 }
-module.exports=Contact;
+
+module.exports = Contact;
--- src/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const contact = require('./model/Contact');
-
-
-
-const contact1 = new contact(
-    "Aa
